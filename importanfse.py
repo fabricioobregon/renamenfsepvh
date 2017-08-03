031342c98d9e5bbf04bb33dea2867cab6e06f63b
Fabricio Obregon <fabricio@serti.com.br>
2017-08-02 21:02:43 -0400

    Organizando nomes de variaveis

--- importanfse.py
+++ importanfse.py
@@ -9,21 +9,21 @@ def importanfse(arquivo):
     emissor = "" 
     nomenota = ""
     #Envia o caminho do arquivo para a funcao lerpdf e salva na variavel line
-    line = lerpdf.lerpdf(arquivo)
+    arquivolido = lerpdf.lerpdf(arquivo)
     #Conta a quantidade de linhas do arquivo
-    counter = len(line)
+    counter = len(arquivolido)
     #Procura os dados nas possiveis linhas do campo e salva nas variaveis
-    for j in range(counter):
-        if str(line[j])[:-12] == "Vencimento":
-            vencimento = str(line[j])[-10:-8]
-        if str(line[j]) == "Competência":
-            competencia = str(line[j+1]).replace("/","")
-        if str(line[j]) == "Número da Nota":
-            nota = str(line[j+2])
-        if str(line[j]) == "CNPJ/CPF":
-            inscfederal = str(line[j+1])
-        if str(line[j]) == "9119":
-            emissor = str(line[j])
+    for linha in range(counter):
+        if str(arquivolido[linha])[:-12] == "Vencimento":
+            vencimento = str(arquivolido[linha])[-10:-8]
+        if str(arquivolido[linha]) == "Competência":
+            competencia = str(arquivolido[linha+1]).replace("/","")
+        if str(arquivolido[linha]) == "Número da Nota":
+            nota = str(arquivolido[linha+2])
+        if str(arquivolido[linha]) == "CNPJ/CPF":
+            inscfederal = str(arquivolido[linha+1])
+        if str(arquivolido[linha]) == "9119":
+            emissor = str(arquivolido[linha])
     #Testa o emissor para formar o nome do arquivo
     if emissor == "9119":
         nomenota = "-NFS" + vencimento + "-" + competencia + ".pdf"
