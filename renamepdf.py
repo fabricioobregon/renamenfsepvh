4f97aa67f4202e2f37d6109a4fdcb7649b8504fd
Fabricio Obregon <fabricio@serti.com.br>
2018-02-25 22:42:17 -0400

    PY-2017.3.3 <fabri@FPO Overwrite remote https://github.com/fabricioobregon/renamenfsepvh to local

--- renamepdf.py
+++
@@ -1,7 +0,0 @@
-from renomeiapdf import renomeiapdf
-from Tkinter import Tk
-from tkFileDialog import askdirectory
-
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-folder = askdirectory() # show an "Open" dialog box and return the path to the selected file
-renomeiapdf(folder+"/")
